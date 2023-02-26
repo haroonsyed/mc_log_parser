26a263d5c8402be85bc325e2bf79a255e2d68772
haroonsyed <haroonaminsyed@gmail.com>
2023-02-26 07:18:39 -0500

    Added filtering of messages. Able to write out filtered log message. (although not run through openai yet)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use std::env;
-use std::error::Error;
-use std::fs::DirEntry;
 use std::fs::{self, File};
 use std::io::{Read, Write};
 use std::path::PathBuf;
 
 use flate2::read::GzDecoder;
+use regex::Regex;
 
 const IN_DIR: &str = "./in";
 const OUT_DIR: &str = "./out";
@@ -73,26 +71,61 @@ fn process_logs() {
 
     // For each file parse it
     for entry in in_dir_info {
-        parse_file_contents(&entry.unwrap());
+        let entry = entry.unwrap();
+        let path = entry.path();
+
+        if has_extension(&path, "log") {
+            let contents = parse_file_contents(&entry);
+            let filtered = filter_log_messages(&contents);
+            save_log(&entry, &filtered);
+        }
     }
 }
 
-fn parse_file_contents(entry: &std::fs::DirEntry) {
-    if entry.file_name() == ".gitignore" {
-        return;
-    }
-
+fn parse_file_contents(entry: &std::fs::DirEntry) -> String {
     let path = entry.path();
     let path_name = path.display().to_string();
 
-    if has_extension(&path, "log") {
-        println!("Parsing File: {path_name}");
+    println!("Parsing File: {path_name}");
 
-        // Get file contents
-        let mut file = File::open(path_name).unwrap();
-        let mut buffer = Vec::new();
-        file.read_to_end(&mut buffer);
-        let contents = String::from_utf8_lossy(&buffer).into_owned();
-        println!("{contents} \n======================================")
-    }
+    // Get file contents
+    let mut file = File::open(path_name).unwrap();
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).unwrap();
+    let contents = String::from_utf8_lossy(&buffer).into_owned();
+    // println!("{contents} \n======================================");
+    return contents;
+}
+
+fn filter_log_messages(messages: &str) -> String {
+    // Split log by timestamp
+    let regex = Regex::new(r"(\[\d{2}:\d{2}:\d{2}\]\s+)").unwrap();
+
+    // Only keep info messages from log
+    let messages: Vec<&str> = regex
+        .split(messages)
+        .filter(|msg| is_info_msg(*msg))
+        .collect();
+
+    // Remove startup info from log
+    let full_log = messages.join("");
+    let split_pos = full_log.find("[Server thread/INFO]: Done");
+    let log = match split_pos {
+        Some(split_pos) => full_log.split_at(split_pos).1,
+        None => &full_log,
+    };
+
+    return String::from(log);
+}
+
+fn is_info_msg(msg: &str) -> bool {
+    return msg.contains("[Server thread/INFO]");
+}
+
+fn save_log(entry: &std::fs::DirEntry, log: &str) {
+    let path = entry.path();
+    let file_name = path.file_stem().unwrap().to_str().unwrap();
+    let out_file_path = OUT_DIR.to_owned() + "/" + file_name + ".log";
+
+    fs::write(out_file_path, log).unwrap();
 }
